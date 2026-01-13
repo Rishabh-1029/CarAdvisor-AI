ca3a839af744aeddd61f2b89b5ef8a2fbc56b653
Rishabh Surana <rishabhjainsurana@gmail.com>
2026-01-13 17:06:19 +0530

    Backend - New Expense Module

---
+++ Backend/services/Expense/expenseuser.py
@@ -0,0 +1,8 @@
+def expenseuser(expense_user_data):
+    return [
+        {"year": "2026", "cost": "100.51"},
+        {"year": "2027", "cost": "112.32"},
+        {"year": "2028", "cost": "125.73"},
+        {"year": "2029", "cost": "133.95"},
+        {"year": "2030", "cost": "139.64"},
+    ]
