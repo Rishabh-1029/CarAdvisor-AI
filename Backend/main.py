ee855c151c963ac6657d93e386e213d98426b7b7
Rishabh Surana <rishabhjainsurana@gmail.com>
2026-01-13 17:07:16 +0530

    Backend - new endpoint - expenseReport

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from services.findcar import findcar
-from services.carlisting import carlisting
+from services.FindCar.findcar import findcar
+from services.CarList.carlisting import carlisting
+from services.Expense.expenseuser import expenseuser
+
 from typing import List
 
 app = FastAPI()
@@ -41,16 +43,30 @@ class CarResponse(BaseModel):
     count: int
     cars: List[Car]
 
+class fuel_cost(BaseModel):
+    year: str
+    cost: str
+
+class ExpenseUser(BaseModel):
+    city: str
+    fuelType: str
+    years: str
+
+class ExpenseUserResponse(BaseModel):
+    status: str
+    count: int
+    fuel_cost: List[fuel_cost]
+
+# Car Recommendation
 @app.post("/process-car-data")
 async def process_car_data(data: CarRequest):
     # Convert to dict
     car_data = data.dict()
     cars = findcar(car_data)
 
-    
-
     return {"status": "success", "count": len(cars), "cars": cars}
 
+# Car listing
 @app.get("/car-listing")
 async def car_listing():
     cars = carlisting()
@@ -59,3 +75,12 @@ async def car_listing():
         "count": len(cars),
         "cars": cars
     }
+
+# Fuel Expense user by City
+@app.post("/expense-user")
+async def process_expense_user_data(data: ExpenseUser):
+    # Convert to dict
+    expense_user_data = data.dict()
+    fuel_cost = expenseuser(expense_user_data)
+
+    return {"status": "success", "count": len(fuel_cost), "fuel_cost": fuel_cost}
