8dec789d1899a0a5d13774e42bb60ea19e700f36
Rishabh Surana <rishabhjainsurana@gmail.com>
2026-01-24 10:53:19 +0530

    Backend - Insurance Logic Update

--- Backend/services/Expense/Aiforecast/insurancecost.py
+++ Backend/services/Expense/Aiforecast/insurancecost.py
@@ -1,5 +1,20 @@
 insurance_rate = 0.028  
-
+def get_insurance_rate(price):
+    rate = 0.0
+    if(price <= 700000):
+        rate = 0.04
+    elif(price > 700000 and price <= 1600000):
+        rate = 0.038
+    elif(price > 1600000 and price <= 3500000):
+        rate = 0.028
+    else:
+        rate = 0.024
+    
+    # if ev: 
+    #     rate -= 0.005
+    
+    return rate
+    
 depreciation = {
         "year1": 1.0,
         "year2": 0.9,
@@ -14,8 +29,11 @@ def calculate_insurance_cost_forecast(min_price, max_price):
     total_max = 0
 
     for year, factor in depreciation.items():
-        min_cost = min_price * insurance_rate * factor
-        max_cost = max_price * insurance_rate * factor
+        min_cost = min_price * get_insurance_rate(min_price) * factor
+        max_cost = max_price * get_insurance_rate(max_price) * factor
+
+        min_cost += 0.18 * min_cost
+        max_cost += 0.18 * max_cost
 
         min_cost = int(round(min_cost, -2))
         max_cost = int(round(max_cost, -2))
